14b4f680b691863580d0111286dc65a841d0f16c
TXPAGES-LLC <mark@txpages.com>
2026-08-06 09:18:24 -0500

    Auto-fix attempt 12

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,6 @@
-// Type declarations for importing global CSS/SCSS/SASS files in TypeScript
-// Allows side-effect imports like: import './globals.css'
 declare module '*.css';
 declare module '*.scss';
 declare module '*.sass';
 declare module '*.module.css';
 declare module '*.module.scss';
 declare module '*.module.sass';
-
-export {};
